4386804d4eec35dda4a9fe29cfaf14c3764f11ec
Ahmad Nura Ahmad <145141115+anali1@users.noreply.github.com>
2023-11-02 10:19:27 +0100

    Add files via upload
    prototype of tukuntawa farms

---
+++ asset/js/main.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+    $('.fa-bars').click(function(){
+        $(this).toggleClass('fa-times')
+        $(this).toggleClass('nav-toggle');
+    });
+
+    $(window).on('scroll load', function(){
+        $('.fa-bars').removeClass('fa-times');
+        $('nav').removeClass('nav-toggle');
+    });
+
+    $('.count').each(function(){
+        var $this = $(this),
+        countTo = $this.attr('data-count');
+        $({countNum : $this.text() }).animate({
+            countNum : countTo
+        },
+        {
+            duratio:5000,
+            step : function() {
+                $this.text(Math.floor(this.countNum)); 
+            },
+            complete : function(){
+                $this.text(this.countNum + '+');
+            }
+        }
+        )        
+    })
+    $('.project').magnificPopUp({
+        delegate:'a',
+        type:'image',
+        gallery:{
+            enabled:true
+        }
+    });
+});
